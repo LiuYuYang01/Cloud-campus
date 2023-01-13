6ebb2de021ab615216e1cfc19128c3710704310c
刘宇阳 <3311118881@qq.com>
2023-01-13 18:54:09 +0800

    侧边栏用户信息绑定

--- components/menu/menu.js
+++ components/menu/menu.js
@@ -1,43 +1,54 @@
-// components/menu/menu.js
+import { storeBindingsBehavior } from "mobx-miniprogram-bindings";
+import store from "../../store/store";
+
 Component({
-    /**
-     * 组件的属性列表
-     */
-    properties: {
-        pageX: Number
+  // 通过storeBindingsBehavior来实现自动绑定
+  behaviors: [storeBindingsBehavior],
+  storeBindings: {
+    store, //指定要绑定的store
+    fields: { //指定要绑定的数据字段或计算属性
+      userInfo: "userInfo",
     },
-
-    /**
-     * 组件的初始数据
-     */
-    data: {
-
+    actions: { //指定要绑定的方法
+      updateUserInfo: "updateUserInfo",
     },
+  },
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    pageX: Number,
+  },
 
-    /**
-     * 组件的方法列表
-     */
-    methods: {
-        // 左侧滑动菜单
-        touchStart(e) {
-            this.setData({
-                startPoint: e.touches[0]
-            })
-        },
-        touchMove(e) {
-            let startPoint = this.data.startPoint.clientX
+  /**
+   * 组件的初始数据
+   */
+  data: {},
 
-            if ((startPoint - e.touches[e.touches.length - 1].clientX) < -100) {
-                let n = e.changedTouches[0].clientX
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    // 左侧滑动菜单
+    touchStart(e) {
+      this.setData({
+        startPoint: e.touches[0],
+      });
+    },
+    touchMove(e) {
+      let startPoint = this.data.startPoint.clientX;
 
-                this.setData({
-                    pageX: n
-                })
-            } else {
-                this.setData({
-                    pageX: 0
-                })
-            }
-        },
-    }
-})
+      if (startPoint - e.touches[e.touches.length - 1].clientX < -100) {
+        let n = e.changedTouches[0].clientX;
+
+        this.setData({
+          pageX: n,
+        });
+      } else {
+        this.setData({
+          pageX: 0,
+        });
+      }
+    },
+  },
+});
