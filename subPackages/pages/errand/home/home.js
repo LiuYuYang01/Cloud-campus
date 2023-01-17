95c800d7108ca07a8cfe5992bbd3367fc7f1da13
刘宇阳 <3311118881@qq.com>
2023-01-17 16:57:38 +0800

    完成校园跑腿首页布局

--- subPackages/pages/errand/home/home.js
+++ subPackages/pages/errand/home/home.js
@@ -9,7 +9,26 @@ Component({
   /**
    * 组件的初始数据
    */
-  data: {},
+  data: {
+    // 轮播图数据
+    swiperList: [
+      {
+        title: "跑腿维权请联系平台管理员",
+        image: "http://img.liuyuyang.net/zhxy/weiquan.jpg",
+        cid: "1",
+      },
+      {
+        title: "避免消费纠纷，权益维护要及时",
+        image: "http://img.liuyuyang.net/zhxy/weiquan1.jpg",
+        cid: "1",
+      },
+      {
+        title: "打击诈骗，从我做起",
+        image: "http://img.liuyuyang.net/zhxy/weiquan2.jpg",
+        cid: "1",
+      },
+    ],
+  },
 
   /**
    * 组件的方法列表
