40b44d9ecd65c2ce21b16c026fa718274eb9e4f4
刘宇阳 <3311118881@qq.com>
2023-01-21 17:46:28 +0800

    完成发布文章插入图片功能

--- components/editor/editor.js
+++ components/editor/editor.js
@@ -1,64 +1,99 @@
 Component({
-    /**
-     * 组件的属性列表
-     */
-    properties: {
+  /**
+   * 组件的属性列表
+   */
+  properties: {},
 
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    delta: {},
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    // 组件初始化
+    onEditorReady() {
+      this.createSelectorQuery()
+        .select("#editor")
+        .context((res) => {
+          this.editorCtx = res.context;
+        })
+        .exec();
     },
+    // 工具栏
+    format(e) {
+      let { name, value } = e.target.dataset;
 
-    /**
-     * 组件的初始数据
-     */
-    data: {
-        delta:{}
+      if (!name) return;
+      this.editorCtx.format(name, value);
     },
+    // 撤销
+    undo() {
+      this.editorCtx.undo();
+    },
+    // 恢复
+    redo() {
+      this.editorCtx.redo();
+    },
+    // 插入图片
+    insertImage() {
+      wx.chooseImage({
+        count: 1,
 
-    /**
-     * 组件的方法列表
-     */
-    methods: {
-        // 组件初始化
-        onEditorReady() {
-            this.createSelectorQuery().select('#editor').context((res) => {
-                this.editorCtx = res.context
-            }).exec()
-        },
-        // 工具栏
-        format(e) {
-            let {
-                name,
-                value
-            } = e.target.dataset
+        success: (res) => {
+          wx.uploadFile({
+            //请求后台的路径
+            url: "https://api.tockey.cn/api/upload",
+            //小程序本地的路径
+            filePath: res.tempFilePaths[0],
+            //后台获取我们图片的key
+            name: "images",
+            //额外的参数formData
+            formData: {
+              type: "article",
+              file: res.tempFilePaths[0],
+            },
 
-            if (!name) return
-            this.editorCtx.format(name, value)
+            success: (res) => {
+              //上传成功
+              console.log(JSON.parse(res.data).data.url, 111);
+              this.editorCtx.insertImage({
+                src: JSON.parse(res.data).data.url,
+                data: {
+                  id: "abcd",
+                  role: "god",
+                },
+                width: "80%",
+                success: function () {
+                  console.log("insert image success");
+                },
+              });
+            },
+            fail: function (res) {
+              console.log(res, 222);
+            },
+          });
         },
-        // 撤销
-        undo() {
-            this.editorCtx.undo()
+      });
+    },
+    // 删除操作
+    clear() {
+      this.editorCtx.clear();
+    },
+    // 发布内容
+    release() {
+      // 获取编辑器内容
+      this.editorCtx.getContents({
+        success: (res) => {
+          this.setData({
+            delta: res,
+          });
         },
-        // 恢复
-        redo() {
-            this.editorCtx.redo()
-        },
-        // 插入图片
-        // insertImage() {
-        //     this.editorCtx.insertImage()
-        // },
-        // 删除操作
-        clear() {
-            this.editorCtx.clear()
-        },
-        // 发布内容
-        release() {
-            // 获取编辑器内容
-            this.editorCtx.getContents({
-                success: (res) => {
-                    this.setData({
-                        delta:res
-                    })
-                }
-            })
-        },
-    }
-})
+      });
+    },
+  },
+});
