0109deb75e478ee052066ffbcd815e5b15e7e886
李晓松 <tockey@yeah.net>
2023-01-31 19:24:59 +0800

    完成：搜索结果样式与数据渲染

--- pages/hobby/hobby.js
+++ pages/hobby/hobby.js
@@ -1,122 +1,150 @@
+import { $http } from "@escook/request-miniprogram";
+import Notify from '@vant/weapp/notify/notify';
 Page({
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    pageX: 0,
-    startPoint: 0,
-    active: 1,
-    swiperList: [], //轮播图列表
-    // 入口导航
-    cateList: [],
-    hobbyToppingList: [],
-    hobbyList: [],
-  },
-  // 左侧滑动菜单
-  touchStart(e) {
-    this.setData({
-      startPoint: e.touches[0],
-    });
-  },
-  touchMove(e) {
-    let startPoint = this.data.startPoint.clientX;
-
-    if (startPoint - e.touches[e.touches.length - 1].clientX < -100) {
-      let n = e.changedTouches[0].clientX;
-
-      this.setData({
-        pageX: n,
-      });
-    } else {
-      this.setData({
+    /**
+     * 页面的初始数据
+     */
+    data: {
         pageX: 0,
-      });
-    }
-  },
-  // 获取轮播图数据
-  async __getSwiperList() {
-    // 获取轮播图
-    const { data: res } = await wx.$http.get("/api/hobby/swiper");
-    this.setData({
-      swiperList: res.data,
-    });
-  },
-  // 获取学校动态置顶文章
-  async __getHomeToppingList() {
-    const { data: res } = await wx.$http.get("/api/article/topping", {
-      type: "hobby",
-    });
+        startPoint: 0,
+        active: 1,
+        swiperList: [], //轮播图列表
+        // 入口导航
+        cateList: [],
+        hobbyToppingList: [],
+        hobbyList: [],
+        searchRes: [], // 搜索结果
+        showSearchBox: false, // 显示搜索结果
+        times: null, // 定时器 防抖用
+        showSearchLod: false, // 正在搜索
+    },
+    showSB() { this.setData({ showSearchBox: true }) },
+    hideSB() { this.setData({ showSearchBox: false }) },
+    searchHandler(e) {
+        clearTimeout(this.data.times)
+        if (!e.detail.length) return   this.setData({searchRes: ''})
+        this.setData({
+            times: setTimeout(() => {
+                this.setData({ showSearchLod: true })
+                wx.request({
+                    url: `https://api.tockey.cn/api/search`,
+                    method: 'post',
+                    data: { value: e.detail },
+                    success: res => {
+                        let { code, message, data } = res.data;
+                        if (code == 400) return Notify(message);
+                        this.setData({ searchRes: data, showSearchLod: false })
+                    }
+                })
+            }, 300)
+        })
+    },
 
-    // 请求失败提示
-    if (res.code !== 200)
-      Notify({
-        type: "danger",
-        message: res.message,
-      });
+    // 左侧滑动菜单
+    touchStart(e) {
+        this.setData({
+            startPoint: e.touches[0],
+        });
+    },
+    touchMove(e) {
+        let startPoint = this.data.startPoint.clientX;
 
-    this.setData({
-      hobbyToppingList: res.data,
-    });
-  },
-  // 获取兴趣圈数据
-  async __getHobbyList() {
-    const { data: res } = await wx.$http.get("/api/hobby/article");
+        if (startPoint - e.touches[e.touches.length - 1].clientX < -100) {
+            let n = e.changedTouches[0].clientX;
 
-    this.setData({
-      hobbyList: res.data,
-    });
-  },
-  // 获取兴趣圈分类数据
-  async cateList() {
-    const {
-      data: { data },
-    } = await wx.$http.get("/api/hobby/cate");
-    this.setData({
-      cateList: data,
-    });
-  },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-    this.__getSwiperList();
-    this.__getHomeToppingList();
-    this.__getHobbyList();
-    this.cateList();
-  },
+            this.setData({
+                pageX: n,
+            });
+        } else {
+            this.setData({
+                pageX: 0,
+            });
+        }
+    },
+    // 获取轮播图数据
+    async __getSwiperList() {
+        // 获取轮播图
+        const { data: res } = await wx.$http.get("/api/hobby/swiper");
+        this.setData({
+            swiperList: res.data,
+        });
+    },
+    // 获取学校动态置顶文章
+    async __getHomeToppingList() {
+        const { data: res } = await wx.$http.get("/api/article/topping", {
+            type: "hobby",
+        });
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {},
+        // 请求失败提示
+        if (res.code !== 200)
+            Notify({
+                type: "danger",
+                message: res.message,
+            });
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {},
+        this.setData({
+            hobbyToppingList: res.data,
+        });
+    },
+    // 获取兴趣圈数据
+    async __getHobbyList() {
+        const { data: res } = await wx.$http.get("/api/hobby/article");
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {},
+        this.setData({
+            hobbyList: res.data,
+        });
+    },
+    // 获取兴趣圈分类数据
+    async cateList() {
+        const {
+            data: { data },
+        } = await wx.$http.get("/api/hobby/cate");
+        this.setData({
+            cateList: data,
+        });
+    },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad(options) {
+        this.__getSwiperList();
+        this.__getHomeToppingList();
+        this.__getHobbyList();
+        this.cateList();
+    },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {},
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady() { },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {},
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow() { },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {},
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide() { },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {},
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload() { },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh() { },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom() { },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage() { },
 });
