11f5107654167569e4a1f7195424d640040da112
李晓松 <tockey@yeah.net>
2023-02-16 14:21:18 +0800

    修复：用户注册时验证码数据双向绑定的问题

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ import store from "./store/store";
 wx.$store = store; // 挂载到全局实例
 
 // 每次小程序启动获取最新的用户数据
-wx.$http.get(`/api/user/${wx.$store.userInfo.id}`).then(res => {
-    wx.$store.updateUserInfo(res.data.data[0]);
-});
+if(wx.$store.userInfo.id) {
+    wx.$http.get(`/api/user/${wx.$store.userInfo.id}`).then(res => {
+        wx.$store.updateUserInfo(res.data.data[0]);
+    });
+}
 
 // app.js
 App({});
