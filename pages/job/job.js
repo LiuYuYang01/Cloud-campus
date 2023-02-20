dc124ca52c2c65a1c0e92e29b1d14554ba1e7f3b
李晓松 <tockey@yeah.net>
2023-02-20 21:12:25 +0800

    完成：兼职页面数据渲染与发布

--- pages/job/job.js
+++ pages/job/job.js
@@ -5,7 +5,22 @@ Page({
    */
   data: {
     active: 1,
+    jobList:[],
+    myJobList:[],
+    // jobForm:{
+        title:'',
+        describe:'',
+        phone:'',
+        price:'',
+        tag:''
+    // }
   },
+
+    //   发布兼职
+    sendJob(){
+        let {title,describe,phone,price,tag} = this.data;
+        wx.$http.post('/api/job',{title,describe,phone,price,tag,uid:wx.$store.userInfo.id})
+    },
 
   onChange(e) {
     console.log(e);
@@ -24,7 +39,22 @@ Page({
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow() {},
+  onShow() {
+      wx.$http.get('/api/job/list').then(res => {
+        //   console.log(res);
+        let {data:{code,data,message}} = res;
+        if (code != 200) return console.error("在获取兼职列表过程中发生错误:",message);
+        this.setData({jobList:data});
+      });
+
+    // 我发布的兼职
+    wx.$http.get(`/api/job/my/${wx.$store.userInfo.id}`).then(res => {
+        //   console.log(res);
+        let {data:{code,data,message}} = res;
+        if (code != 200) return console.error(message);
+        this.setData({myJobList:data});
+      });
+  },
 
   /**
    * 生命周期函数--监听页面隐藏
