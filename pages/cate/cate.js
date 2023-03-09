1d8409c9f2ea193a06be1c35c55769b3b3a9d38d
刘宇阳 <3311118881@qq.com>
2023-03-09 17:53:23 +0800

    完成兴趣圈分类功能

--- pages/cate/cate.js
+++ pages/cate/cate.js
@@ -1,66 +1,74 @@
 // pages/cate/cate.js
 Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    cateList: [],
+  },
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
+  // 重新获取最新数据
+  async getList() {
+    // 普通文章
+    const {
+      data: { code, data },
+    } = await wx.$http.get(`/api/hobby/article`);
 
-    },
+    if (code !== 200) return;
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad(options) {
+    this.setData({
+      cateList: data.filter((item) => {
+        return item.cate === "足球社";
+      }),
+    });
 
-    },
+    console.log(this.data.cateList);
+  },
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady() {
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+    this.getList();
+  },
 
-    },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {},
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow() {
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {},
 
-    },
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {},
 
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide() {
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {},
 
-    },
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+    this.getList()
+    
+    // 关闭下拉刷新
+    wx.stopPullDownRefresh();
+  },
 
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload() {
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {},
 
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh() {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom() {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage() {
-
-    }
-})
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {},
+});
