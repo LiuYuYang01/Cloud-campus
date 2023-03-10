3155e4e5acc6a8a328cd1b521d064dd495702f90
刘宇阳 <3311118881@qq.com>
2023-03-10 17:00:29 +0800

    完成机器人复制内容功能

--- subPackages/pages/robot/robot.js
+++ subPackages/pages/robot/robot.js
@@ -77,6 +77,22 @@ Page({
 
     wx.hideLoading();
   },
+  // 内容复制
+  viewCopyTextClick() {
+    var _this = this;
+    wx.setClipboardData({
+      data: _this.data.viewCopyMsg,
+      success: function (res) {
+        wx.getClipboardData({
+          success: function (res) {
+            wx.showToast({
+              title: "复制成功",
+            });
+          },
+        });
+      },
+    });
+  },
 
   /**
    * 生命周期函数--监听页面加载
