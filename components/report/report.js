a80f5471efa0cc361ed4fcfbb5c555aac277e6ac
刘宇阳 <3311118881@qq.com>
2023-03-30 18:05:49 +0800

    完成每个页面名称的动态渲染

--- components/report/report.js
+++ components/report/report.js
@@ -14,8 +14,16 @@ Component({
 
   lifetimes: {
     ready() {
-      console.log(this.properties.data, 222);
-      console.log(this.properties, 333);
+        // 拿到小程序的上级所有页面
+        const pages = getCurrentPages();
+        
+        // 然后找到最后一个，也就是刚刚跳转过来的页面
+        const currentPage = pages[pages.length - 1];
+
+        // 拿到他的跳转参数：option.title 来动态设置每个页面的名称
+        wx.setNavigationBarTitle({
+          title: currentPage.options.title,
+        });
     },
   },
 
